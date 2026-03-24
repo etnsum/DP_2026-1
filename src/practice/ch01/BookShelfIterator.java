e44930d1cc626779b426fe648c94d96ba62d0e55
summer <etnsumm@gmail.com>
2026-03-24 09:13:20 +0900

    0324class

---
+++ src/practice/ch01/BookShelfIterator.java
@@ -0,0 +1,33 @@
+package practice.ch01;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class BookShelfIterator implements Iterator<Book> {
+    private BookShelf bookShelf;
+    private int index; // 현재 위치
+
+    public BookShelfIterator(BookShelf bookShelf) {
+        this.bookShelf = bookShelf;
+        this.index = 0;
+    }
+
+    @Override
+    public boolean hasNext() { //책꽂이에서 꺼내올 다음 책이 있는지 확인하는 메소드
+        if (index < bookShelf.getLength()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    @Override
+    public Book next() { //책꽂이에서 다음 책을 꺼내오는 메소드
+        if (!hasNext()) {
+            throw new NoSuchElementException(); // 더 이상 꺼내올 책이 없을 때 예외 처리
+        }
+        Book book = bookShelf.getBookAt(index); // 현재 위치의 책을 가져옴
+        index++;
+        return book;
+    }
+}
--- src/practice/ch01/Main.java
+++ src/practice/ch01/Main.java
@@ -1,8 +1,34 @@
 package practice.ch01;
 
+import java.util.Iterator;
+
 public class Main {
     public static void main(String[] args){
-        Book book = new Book(name: "Java Programming");
+        Book book = new Book("Java Programming");
         System.out.println("Book name: " + book.getName());
+
+        BookShelf bookShelf = new BookShelf(5);
+        bookShelf.appendBook(new Book("Java Programming"));
+        bookShelf.appendBook(new Book("Android Programming"));
+        bookShelf.appendBook(new Book("C Programming"));
+
+
+        // 방법1: 클라이언트가 직접 책장에 접근하여 책을 가져오는 방법
+        for (int i = 0; i < bookShelf.getLength(); i++) {
+            System.out.println("Book " + ( i + 1) + ": " + bookShelf.getBookAt(i).getName());
+        }
+
+        // 방법2: iterator를 이용하여 책을 가져오는 방법
+        // Iterator를 얻어옴
+        Iterator<Book> it = bookShelf.iterator();
+        while(it.hasNext()) { // 다음 책이 있는지 확인
+            System.out.println("Book: " + it.next().getName()); // 다음 책을 가져옴
+        }
+
+        // 확장 for문을 이용하여 책을 가져오는 방법
+        // 이게 가능한 이유는 BookShelf가 Iterable 인터페이스를 구현했기 때문
+        for (Book b : bookShelf) { // 형식: for (타입 변수명 : 컬렉션) { ... }
+            System.out.println("Book: " + b.getName());
+        }
     }
 }

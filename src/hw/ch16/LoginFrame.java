ed443cd1aa2450fdc4295f51031d685f459f0dec
etnsum <etnsumm@gmail.com>
2026-05-19 17:08:26 +0900

    hw

---
+++ src/hw/ch16/LoginFrame.java
@@ -0,0 +1,131 @@
+package hw.ch16;
+
+import java.awt.CheckboxGroup;
+import java.awt.Color;
+import java.awt.Frame;
+import java.awt.GridLayout;
+import java.awt.Label;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class LoginFrame extends Frame implements ActionListener, Mediator {
+    private ColleagueCheckbox checkGuest;
+    private ColleagueCheckbox checkLogin;
+    private ColleagueTextField textUser;
+    private ColleagueTextField textPass;
+    private ColleagueButton buttonOk;
+    private ColleagueButton buttonCancel;
+
+
+    // 상태 표시 Label 추가
+    private ColleagueLabel statusLabel;
+
+    public LoginFrame(String title) {
+        super(title);
+
+        setBackground(Color.lightGray);
+        setLayout(new GridLayout(4, 2));
+
+        // Colleague 생성한
+        createColleagues();
+
+
+        add(checkGuest);
+        add(checkLogin);
+        add(new Label("Username:"));
+        add(textUser);
+        add(new Label("Password:"));
+        add(textPass);
+        add(buttonOk);
+        add(buttonCancel);
+
+        // 마지막 행에 상태 Label 배치
+        add(statusLabel);
+        add(new Label(""));
+
+
+        colleagueChanged();
+
+
+        pack();
+        setVisible(true);
+    }
+
+
+    @Override
+    public void createColleagues() {
+
+        CheckboxGroup g = new CheckboxGroup();
+        checkGuest = new ColleagueCheckbox("Guest", g, true);
+        checkLogin = new ColleagueCheckbox("Login", g, false);
+
+        textUser = new ColleagueTextField("", 10);
+        textPass = new ColleagueTextField("", 10);
+        textPass.setEchoChar('*');
+
+        buttonOk = new ColleagueButton("OK");
+        buttonCancel = new ColleagueButton("Cancel");
+
+        // 상태 Label 생성
+        statusLabel = new ColleagueLabel("● 로그인 불가");
+
+        checkGuest.setMediator(this);
+        checkLogin.setMediator(this);
+        textUser.setMediator(this);
+        textPass.setMediator(this);
+        buttonOk.setMediator(this);
+        buttonCancel.setMediator(this);
+
+        // statusLabel Mediator 등록
+        statusLabel.setMediator(this);
+
+        checkGuest.addItemListener(checkGuest);
+        checkLogin.addItemListener(checkLogin);
+        textUser.addTextListener(textUser);
+        textPass.addTextListener(textPass);
+        buttonOk.addActionListener(this);
+        buttonCancel.addActionListener(this);
+    }
+
+    @Override
+    public void colleagueChanged() {
+        if (checkGuest.getState()) {
+            // 게스트 로그인 
+            textUser.setColleagueEnabled(false);
+            textPass.setColleagueEnabled(false);
+            buttonOk.setColleagueEnabled(true);
+
+            // 연동
+            statusLabel.setColleagueEnabled(true);
+        } else {
+            // 사용자 로그인 
+            textUser.setColleagueEnabled(true);
+            userpassChanged();
+        }
+    }
+
+    private void userpassChanged() {
+        if (textUser.getText().length() > 0) {
+            textPass.setColleagueEnabled(true);
+             if (textUser.getText().length() >= 4 && textPass.getText().length() >= 4) {
+                buttonOk.setColleagueEnabled(true);
+                statusLabel.setColleagueEnabled(true);
+            } else {
+                buttonOk.setColleagueEnabled(false);
+                // 연동
+                statusLabel.setColleagueEnabled(false);
+            }
+        } else {
+            textPass.setColleagueEnabled(false);
+            buttonOk.setColleagueEnabled(false);
+            // 연동
+            statusLabel.setColleagueEnabled(false);
+        }
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        System.out.println(e.toString());
+        System.exit(0);
+    }
+}
